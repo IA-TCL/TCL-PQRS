0b653e081f028967ebaebc9e6f795239b4e822b2
YeilerTCL <yeiler.rendon@tclasesores.com>
2026-06-18 10:30:46 -0500

    security: agregar rate limiting 5 envios por hora por IP

--- server.py
+++ server.py
@@ -5,11 +5,17 @@ from urllib.parse import quote
 from typing import Optional
 
 import requests
-from fastapi import FastAPI, Form, File, UploadFile
+from fastapi import FastAPI, Form, File, UploadFile, Request
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 
+limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
@@ -29,7 +35,9 @@ async def health():
 
 
 @app.post("/pqrs")
+@limiter.limit("5/hour")
 async def submit_pqrs(
+    request:          Request,
     nombres:          str = Form(...),
     apellidos:        str = Form(...),
     correo:           str = Form(...),
